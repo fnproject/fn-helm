e7d72cd2c1fe0c0e0f3ba7f446c2e7bac7f5c434
Owen Cliffe <owen.cliffe@oracle.com>
2017-11-30 19:11:40 +0000

    Removing tests, putting helm lint in ci

--- tests/test_helper.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-function teardown(){
-    for rel in $(helm ls -q | grep testcluster-) ; do
-        echo deleting release ${rel}
-        helm del --purge ${rel}
-    done
-}
